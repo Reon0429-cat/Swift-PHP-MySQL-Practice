8c83df504d7328e173b59af668f2cc26a28d84b0
Reon0429-cat <oonishireon110@gmail.com>
2021-12-07 22:45:44 +0900

    localhostにリクエストを送れるようにした

--- Swift-PHP-MySQL-Practice/ViewController.swift
+++ Swift-PHP-MySQL-Practice/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
+    
+    @IBAction private func sendButtonDidTapped(_ sender: Any) {
+        guard let url = URL(string: "http://localhost/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        guard let text = textField.text else { return }
+        let postParameters = "text=\(text)"
+        request.httpBody = postParameters.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("DEBUG_PRINT: ", error.localizedDescription)
+                return
+            }
+            guard let data = data else { return }
+            do {
+                let user = try JSONDecoder().decode(User.self, from: data)
+                print("DEBUG_PRINT: ", user.text)
+            } catch {
+                print("DEBUG_PRINT: ", error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
 }
 
+struct User: Decodable {
+    
+    let text: String
+    
+}
